c7ee27f205a68255cfc4253d6c287ac191942cbc
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2021-10-29 18:09:45 +0100

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,70 +2,103 @@
 const viewer = document.getElementById('viewer')
 const button = document.getElementById('button')
 const another = document.getElementById('another')
+const showcase = document.getElementById('showcase')
+const tvMessage = document.getElementById('tv-message')
+const attribution = document.getElementById('attribution')
 
 let emailsMap = new Map()
+let currentResponse
 
-// on window load place a random image into the viewer
-window.onload = () => {
-  getImage()
-  .then((response) => {
-    viewer.innerHTML = `<img src="${response.urls.small}?&h=500&w=700" alt="${response.alt_description}">`
-    return response
+// START listeners
+window.onload = () => {  // on page load
+  getImage()  // get a new image
+  .then((response) => {  // populate the viewer with the image
+    viewer.innerHTML = `<img src="${response.urls.small}?&h=400&w=575" alt="${response.alt_description}">`
+    attribution.innerHTML = `<span>Photo taken by <a href="${response.user.links.html}?utm_source=netmatters%20task&utm_medium=referral" target="_blank">${response.user.name}</a></span>`
+    currentResponse = response  // to pass the response to functions like addImageToEmail
   })
-  .then((response) => {
-    addImageToEmail(response)
-  })
-  // .then((response) => {
-  
-  // })
   .catch((error) => {
     console.log('There was an error' + error)
-    alert('There was an error: ' + error)
+    error = "Error 403"? tvMessage.innerHTML = '<span>Run out of image requests, sorry. Try again soon</span>' : '<span>Sorry, the programme ran into an error. Developers should check the console log for error messages.</span>'
   })
 }
 
-another.addEventListener('click', () =>{
-  getImage()
-  .then((response) => {
-    viewer.innerHTML = `<img src="${response.urls.small}?&h=500&w=700" alt="${response.alt_description}">`
-    return response
+another.addEventListener('click', () =>{  // when a user clicks the "give me another image" button
+  getImage()  // get another image
+  .then((response) => {  // populate the viewer with the new image
+    viewer.innerHTML = `<img src="${response.urls.small}?&h=400&w=575" alt="${response.alt_description}">`
+    attribution.innerHTML = `<span>This photo was taken by <a href="${response.user.links.html}?utm_source=netmatters%20task&utm_medium=referral" target="_blank">${response.user.name}</a></span>`
+    currentResponse = response  // to pass the response to functions like addImageToEmail
   })
-  .then((response) => {
-    addImageToEmail(response)
+  .then(() => {
+    tvMessage.innerHTML = "";
   })
   .catch((error) => {
     console.log('There was an error' + error)
-    alert('There was an error: ' + error)
+    error = "Error 403"? tvMessage.innerHTML = '<span>Run out of image requests, sorry. Try again soon</span>' : '<span>Sorry, the programme ran into an error. Developers should check the console log for error messages.</span>'
   })
 })
 
-function addImageToEmail(response){
-  return new Promise((resolve, reject) => {
-    button.addEventListener('click', () => {
-      const email = document.getElementById('email').value
-      if(email !=""){  // if something is in the email textarea
-        if(emailsMap.has(email)){  // if the email KEY exists
-          console.log('first state triggered')
-          let imagesArray = new Array()
-          imagesArray = emailsMap.get(email)  // retrieve the VALUE array from the map 
-          if(imagesArray.indexOf(response.id) === -1){  // if the image id is not already in the imagesArray
-            console.log('id does not exist in array')
-            imagesArray.unshift(response.id)
-            emailsMap.set(email, imagesArray)
-            resolve()
-          }
-        }else{  // the email KEY does not exist
-          console.log('second state triggered')
-          // email key does not exist add email as the key and add the image url to the beginning of the value array.
-          let imagesArray = new Array()
-          imagesArray.unshift(response.id)
-          emailsMap.set(email, imagesArray)
-          resolve()
-        }
-      }
-    })
-  })
+
+button.addEventListener('click', () => {  // when a user clicks the "send it to me" button
+  const email = document.getElementById('email').value
+  if(validateEmail(email)){
+    addImageToEmail(currentResponse, email)
+  }
+  displayCollection()
+})
+
+// END listeners
+
+//START functions
+
+function validateEmail(email){
+  const regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if(!regex.test(email)){
+    tvMessage.style.display = "inline-flex";
+    tvMessage.innerHTML = "The email address you supplied is not a valid email address. Please check your entry.\n";
+  }else{
+    tvMessage.innerHTML = "";
+    tvMessage.style.display = "none";
+    return true
+  }
 }
+
+function addImageToEmail(response, email){
+  if(email !=""){  // if something is in the email textarea
+    if(emailsMap.has(email)){  // if the email KEY exists
+      let imagesArray = new Array()
+      imagesArray = emailsMap.get(email)  // retrieve the VALUE array from the map 
+      if(imagesArray.indexOf(response.urls.thumb) === -1){  // if the image id is not already in the imagesArray
+        imagesArray.unshift(response.urls.thumb)  // add the image id to the beginning of the array
+        emailsMap.set(email, imagesArray)  // add the image array as the VALUE onto the associated email KEY
+      }
+    }else{  // the email KEY does not exist
+      let imagesArray = new Array()
+      imagesArray.unshift(response.urls.thumb)  // add the image id to the beginning of the image array
+      emailsMap.set(email, imagesArray)  // create a new map entry with the email KEY and add the image array as the VALUE
+    }
+  }
+}
+
+
+function displayCollection(){
+  let output = ""  // setup a discard variable for use as a concatenation tool for output
+  const tempArray = Array.from(emailsMap.keys());  // make a temp array from the map keys so we can reverse it
+  const sizeAffix = "?&h=100&w=300"
+  tempArray.reverse();
+  for(let key of tempArray){  // loop through emails in reverse order (most recent first)
+    const urls = emailsMap.get(key)  // get the array of urls stored as the VALUE in the emailsMap for this email
+    output += `<h2 class="email-tag">${key}</h2><br>`   // start to build the output string by adding the email (once!)
+    for(let src of urls){         // loop through each url
+      output += `<img src="${src}${sizeAffix}" class="thumb" alt="thumbnail preview">`    // add it to the image tag
+    }
+  }
+  showcase.innerHTML = output   // add the concatenated output to the inside of the showcase div 
+}
+// END functions
+
+// START promises
 
 function getImage(){
   return new Promise((resolve, reject) => {
@@ -88,7 +121,7 @@ function getImage(){
     };
     
     xhr.onprogress = function(event) {
-      viewer.style.backgroundImage = 'url(\'/assets/img/tvStatic.jpg\')' // not working? try later
+      viewer.style.backgroundImage = "url('../img/tvStatic.jpg')" // not working? try later
     };
     
     xhr.onerror = function() {
@@ -97,3 +130,4 @@ function getImage(){
   })
 }
 
+// END promises
